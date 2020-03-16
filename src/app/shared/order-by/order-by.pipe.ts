148e417213a8bdb0c96b7be3796d3d68c6bb7f81
stLmpp <gui.stlmpp@hotmail.com>
2020-03-16 00:02:34 -0300

    - adicionado opção no pipe orderBy para ordenar por mais de uma coluna

--- src/app/shared/order-by/order-by.pipe.ts
+++ src/app/shared/order-by/order-by.pipe.ts
@@ -1,11 +1,11 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { Order, orderBy, OrderByType } from './order-by';
+import { Order, orderBy } from './order-by';
 
 @Pipe({ name: 'orderBy' })
 export class OrderByPipe implements PipeTransform {
-  transform<T>(
+  transform<T, K extends keyof T = keyof T>(
     array: T[],
-    keyOrCommand?: OrderByType<T>,
+    keyOrCommand?: K[] | K | ((valueA: T, valueB: T) => number),
     order: Order = 'asc'
   ): T[] {
     return orderBy(array, keyOrCommand, order);
--- src/app/shared/order-by/order-by.ts
+++ src/app/shared/order-by/order-by.ts
@@ -1,34 +1,67 @@
-import { isFunction, isNumber } from 'is-what';
+import { isArray, isFunction, isNumber } from 'is-what';
 import { isNil } from '../../util/util';
 
 export type Order = 'asc' | 'desc';
-export type OrderByType<T> = keyof T | ((valueA: T, valueB: T) => number);
 
-export function orderBy<T>(
+export type OrderByType<T, K extends keyof T = keyof T> =
+  | K[]
+  | K
+  | ((valueA: T, valueB: T) => number);
+
+export function orderBy<T, K = keyof T>(
   values: T[],
   keyOrCommand?: OrderByType<T>,
   order: Order = 'asc'
 ): T[] {
   if (!values?.length) return values;
-  if (!keyOrCommand) return values.sort();
+  if (!keyOrCommand) return values;
   if (isFunction(keyOrCommand)) {
-    return values.sort(keyOrCommand);
+    return [...values].sort(keyOrCommand);
   } else {
-    return values.sort((valueA, valueB) => {
+    return [...values].sort((valueA, valueB) => {
       if (order === 'asc') {
-        return compareValues(valueA, valueB, keyOrCommand);
+        return isArray(keyOrCommand)
+          ? compareValuesMultipleKeys(valueA, valueB, keyOrCommand)
+          : compareValuesKey(valueA, valueB, keyOrCommand);
       } else {
-        return compareValues(valueB, valueA, keyOrCommand);
+        return isArray(keyOrCommand)
+          ? compareValuesMultipleKeys(valueB, valueA, keyOrCommand)
+          : compareValuesKey(valueB, valueA, keyOrCommand);
       }
     });
   }
 }
 
-function compareValues<T>(valueA: T, valueB: T, key: keyof T): number {
-  if (isNil(valueA?.[key]) || isNil(valueB?.[key])) return;
-  if (isNumber(valueA[key]) && isNumber(valueB[key])) {
-    return +valueA[key] - +valueB[key];
+export function compareValues<T>(valueA: T, valueB: T): number {
+  if (isNil(valueA)) return 1;
+  if (isNil(valueB)) return -1;
+  if (isNumber(valueA) && isNumber(valueB)) {
+    return +valueA - +valueB;
   } else {
-    return valueA[key].toString().localeCompare(valueB[key].toString());
+    return valueA.toString().localeCompare(valueB.toString());
   }
+}
+
+export function compareValuesKey<T>(
+  valueA: T,
+  valueB: T,
+  key: keyof T
+): number {
+  return compareValues(valueA?.[key], valueB?.[key]);
+}
+
+export function compareValuesMultipleKeys<T, K extends keyof T = keyof T>(
+  valueA: T,
+  valueB: T,
+  keys: K[]
+): number {
+  let result: number;
+  for (let i = 0, len = keys.length; i < len; i++) {
+    const key = keys[i];
+    if (valueA?.[key] !== valueB?.[key]) {
+      result = compareValuesKey<T>(valueA, valueB, key);
+      break;
+    }
+  }
+  return result;
 }
